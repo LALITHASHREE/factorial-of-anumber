e26fe39957c852cde29b30c2811b98b245386345
LALITHASHREE <dlalithashree@gmail.com>
2016-07-16 15:13:46 +0530

    finish

---
+++ README.c
@@ -0,0 +1,16 @@
+# factorial-of-anumber
+#include<stdio.h>
+#iinclude<conio.h>
+int main()
+{
+int a,b,c;
+clrscr();
+printf("enter the number of a and b");
+scanf("%d%d",&a,&b");
+for(a=0;a<=20;a++)
+{
+c=a+b;
+printf("%d":c);
+}
+return 0;
+}
